66ba9fa3543b0b1467085931afaff5154d3f0d52
joakmannn <165683883+joakmannn@users.noreply.github.com>
2025-08-13 17:48:17 +0200

    Add routes for Hello and Projects pages

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,15 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return Inertia::render('Welcome');
+});
+
+Route::get('/hello', function () {
+    return Inertia::render('Hello');
+});
+
+Route::get('/projects', function () {
+    return Inertia::render('Projects');
 });
 
 Route::get('/dashboard', function () {
