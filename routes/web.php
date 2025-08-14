852062077785c589735da54bf7182b8f60934601
joakmannn <165683883+joakmannn@users.noreply.github.com>
2025-08-14 02:37:55 +0200

    Add CV route

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/projects', function () {
     return Inertia::render('Projects');
 });
 
+Route::get('/cv', function () {
+    return Inertia::render('CV');
+});
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
